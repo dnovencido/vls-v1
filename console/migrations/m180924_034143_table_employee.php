e4ff3436693b12df448aef76b338ee601125fcab
Denver <denver@Denvers-MBP.lan>
2018-09-26 20:36:10 +0800

    Modified migration for table employee

--- console/migrations/m180924_034143_table_employee.php
+++ console/migrations/m180924_034143_table_employee.php
@@ -12,30 +12,41 @@ class m180924_034143_table_employee extends Migration
      */
     public function safeUp()
     {
-        $tables = Yii::$app->db->schema->getTableNames();
+        $tables = Yii::$app->db->schema->getTableNames();ß  
         $dbType = $this->db->driverName;
         $tableOptions_mysql = "CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB";
         $tableOptions_mssql = "";
         $tableOptions_pgsql = "";
         $tableOptions_sqlite = "";
-
+        
         /* MYSQL */
-        if (!in_array('office', $tables))  { 
+
+        if (!in_array('employee', $tables))  { 
             if ($dbType == "mysql") {
-                $this->createTable('{{%office}}', [
-                    'id' => 'INT(11) NOT NULL AUTO_INCREMENT',
+                $this->createTable('{{%employee}}', [
+                    'id' => 'INT(11) UNSIGNED NOT NULL AUTO_INCREMENT',
                     0 => 'PRIMARY KEY (`id`)',
-                    'office_desc' => 'VARCHAR(255) NULL',
+                    'fname' => 'VARCHAR(55) NULL',
+                    'lname' => 'VARCHAR(55) NULL',
+                    'address' => 'VARCHAR(255) NULL',
+                    'office' => 'INT(11) NULL',
+                    'position' => 'INT(11) NULL',
+                    'mobile_no' => 'VARCHAR(255) NULL',
                 ], $tableOptions_mysql);
             }
         }
          
+         
+        $this->createIndex('idx_position_46_00','employee','position',0);
+        
+        $this->createIndex('idx_office_46_01','employee','office',0);
+         
         $this->execute('SET foreign_key_checks = 0');
-
-        $this->insert('{{%office}}',['id'=>'2','office_desc'=>'Region 1']);
-
-        $this->insert('{{%office}}',['id'=>'4','office_desc'=>'Head Office']);
-
+        
+        $this->addForeignKey('fk_position_46_00','{{%employee}}', 'position', '{{%position}}', 'id', 'CASCADE', 'NO ACTION' );
+        
+        $this->addForeignKey('fk_office_46_01','{{%employee}}', 'office', '{{%office}}', 'id', 'CASCADE', 'NO ACTION' );
+        
         $this->execute('SET foreign_key_checks = 1;');
     }
 
@@ -44,12 +55,11 @@ class m180924_034143_table_employee extends Migration
      */
     public function safeDown()
     {
-
         $this->execute('SET foreign_key_checks = 0');
-
+        
         $this->execute('DROP TABLE IF EXISTS `employee`');
-
-        $this->execute('SET foreign_key_checks = 1;');       
+        
+        $this->execute('SET foreign_key_checks = 1;');
          
         echo "m180924_034143_table_employee cannot be reverted.\n";
 
